ca8839a2f23d4b90eb730dfb3ddfde5f90174a1e
AnshB0712 <root@LAPTOP-2AEKO0IQ.localdomain>
2024-09-01 13:57:54 +0530

    2 commit

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 import express from "express"
+import { exec } from "child_process"
+import path from "path"
 const app = express()
 
 app.use(express.json())
 
 app.post("/webhook", (req, res) => {
   const payload = req.body
+  const url = payload.url
 
-  console.log(payload)
+  res.sendStatus(200)
 
-  return res.sendStatus(200)
+  if (payload.ref === "refs/heads/main") {
+    exec(
+      `bash ${path.join(__dirname, "script.sh")} ${url}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error: ${error.message}`)
+          return
+        }
+        if (stderr) {
+          console.error(`Stderr: ${stderr}`)
+          return
+        }
+        console.log(stdout)
+      }
+    )
+  }
 })
 
 app.listen(5000, () => console.log("server started"))
