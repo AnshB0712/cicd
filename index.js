b8d49173c96ba3e2e3920e5c22601a67c55e0491
AnshB0712 <root@LAPTOP-2AEKO0IQ.localdomain>
2024-09-01 14:01:24 +0530

    4 commit

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from "express"
 import { exec } from "child_process"
 import path from "path"
+import { fileURLToPath } from "url"
+
 const app = express()
+
+// Get __dirname equivalent
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+console.log(__dirname)
 
 app.use(express.json())
 
@@ -9,11 +17,11 @@ app.post("/webhook", (req, res) => {
   const payload = req.body
   const url = payload.url
 
+  // Send the response immediately
   res.sendStatus(200)
 
-  console.log("send to webhook 200")
-
   if (payload.ref === "refs/heads/main") {
+    // Execute the shell script asynchronously
     exec(
       `bash ${path.join(__dirname, "script.sh")} ${url}`,
       (error, stdout, stderr) => {
